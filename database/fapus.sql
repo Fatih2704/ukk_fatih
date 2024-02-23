03259dd4165b4742889c9a1c62856870e9de9e3e
fatih2704 <jugobodrex@gmail.com>
2024-02-23 13:16:17 +0700

    db baru(2)

--- database/fapus.sql
+++ database/fapus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 23 Feb 2024 pada 06.57
+-- Waktu pembuatan: 23 Feb 2024 pada 07.15
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
